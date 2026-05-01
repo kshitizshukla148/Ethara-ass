2ff82cfb398547b78bb4e3a27bbe9be9b404d770
kshitiz shukla <kshitizshukla148@gmail.com>
2026-05-02 00:05:37 +0530

    remove options wildcard crash

--- server/src/app.js
+++ server/src/app.js
@@ -20,7 +20,7 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
-app.options("*", cors()); // ✅ ADD THIS LINE
+
 app.use(express.json());
 app.use(morgan("dev"));
 
